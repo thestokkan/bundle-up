77457f80ec7aed7c880e0c430657a2a433276136
Therese Stokkan <thestokkan@gmail.com>
2023-01-15 23:07:10 +0100

    Initial commit - Spring Boot and React App

---
+++ src/main/java/com/bundleup/BundleUpApplication.java
@@ -0,0 +1,13 @@
+package com.bundleup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundleUpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BundleUpApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bundleup/BundleUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bundleup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundleUpApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
