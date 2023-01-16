51ef843448368d8e4e1a4681b8c66b67d8bcfc5a
Therese Stokkan <thestokkan@gmail.com>
2023-01-16 10:30:25 +0100

    Added H2

--- src/main/java/com/bundleup/controllers/TestController.java
+++ src/main/java/com/bundleup/controllers/TestController.java
@@ -2,12 +2,13 @@ package com.bundleup.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
   @GetMapping("/")
   public String home() {
-    return "home";
+    return "Hello!";
   }
 }
