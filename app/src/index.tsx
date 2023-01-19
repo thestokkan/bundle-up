b5d3392940efbcad8d6e32a42aedbfec63263a4c
Daniel <danielwoldegiorgis@hotmail.com>
2023-01-19 16:49:12 +0100

    fix react animation fixed rebase

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
     // <React.StrictMode>
         <ThemeContextProvider>
-            <App />
+            <Clothes />
         </ThemeContextProvider>
     // </React.StrictMode>
 );
