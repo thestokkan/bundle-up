ae101afb9c4b457076f4f245b5d9c7984c76d75f
Therese Stokkan <thestokkan@gmail.com>
2023-01-19 17:46:04 +0100

    move CLothes tag to App.tsx and App back to index.tsx

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
     // <React.StrictMode>
         <ThemeContextProvider>
-            <Clothes />
+            <App />
         </ThemeContextProvider>
     // </React.StrictMode>
 );
