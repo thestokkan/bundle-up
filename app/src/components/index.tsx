ad161b3f5479ad22a26e6db6eac048616a6a4fec
Therese Stokkan <thestokkan@gmail.com>
2023-01-20 12:13:18 +0100

    Move unused components to separate folder and create new file with all original code from Therese's App.tsx. Remove unused code in App.tsx

--- app/src/components/index.tsx
+++ app/src/components/index.tsx
@@ -1,8 +1,8 @@
 export {Button, ButtonWithCSS, CrossButton} from './Buttons';
 export {Link} from './Link';
 export {default as Input} from './InputField';
-export {Slider} from './Slider';
+export {Slider} from '../_unused_components/Slider';
 export {Modal} from './Modal';
 export {Counter} from './Counter';
-export {Toggle} from './Toggle';
+export {Toggle} from '../_unused_components/Toggle';
 export {WeatherPlot} from './PlotWeatherData';
