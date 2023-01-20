29602ff744fed90c15006d59d4c8ab0e971ba703
Therese Stokkan <thestokkan@gmail.com>
2023-01-20 17:38:15 +0100

    more layout

---
+++ app/src/components/DisplayWeather/BasicWeatherData.tsx
@@ -0,0 +1,11 @@
+import './DisplayWeather.css';
+import {getWeatherDataAndClothesCombo} from "../../fetchData";
+import {useEffect, useState} from "react";
+
+const BasicWeather = () => {
+
+
+
+};
+
+export default BasicWeather;
